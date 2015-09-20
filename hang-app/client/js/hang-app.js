1c9069fe46ca517e1b953b373a5356b74fed2475
Pearl Sortman <pearlsortman@gmail.com>
2015-09-19 18:16:52 -0700

    Shifted login dets to template

--- hang-app/client/js/hang-app.js
+++ hang-app/client/js/hang-app.js
@@ -1,26 +1,53 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+    // counter starts at 0
+    Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+    Template.hello.helpers({
+        counter: function() {
+            return Session.get('counter');
+        }
+    });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+    Template.hello.events({
+        'click button': function() {
+            // increment the counter when button is clicked
+            Session.set('counter', Session.get('counter') + 1);
+        }
+    });
 
-  Template.landingPage.helpers({
-  });
+    Template.landingPage.helpers({});
+
+    Template.foo.helpers({
+        templateGestures: {
+            'swipeleft ul li': function(event, templateInstance) {
+                /* `event` is the Hammer.js event object */
+                /* `templateInstance` is the `Blaze.TemplateInstance` */
+                /* `this` is the data context of the element in your template, so in this case `someField` from `someArray` in the template */
+            }
+        }
+    });
+
+    Template.login.events({
+        'click #facebook-login': function(event) {
+            Meteor.loginWithFacebook({}, function(err) {
+                if (err) {
+                    throw new Meteor.Error("Facebook login failed");
+                }
+            });
+        },
+
+        'click #logout': function(event) {
+            Meteor.logout(function(err) {
+                if (err) {
+                    throw new Meteor.Error("Logout failed");
+                }
+            })
+        }
+    });
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    Meteor.startup(function() {
+        // code to run on server at startup
+    });
 }
---
+++ hang-app/server/social-config.js
@@ -0,0 +1,9 @@
+ServiceConfiguration.configurations.remove({
+    service: 'facebook'
+});
+ 
+ServiceConfiguration.configurations.insert({
+    service: 'facebook',
+    appId: '1001911333184728',
+    secret: 'c5fea71335dd5e3c726f9c849302ff66'
+});
